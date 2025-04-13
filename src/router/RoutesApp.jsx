203f2d2e7edcce2670bc93fb2388a089ec527e75
Lessly_Barba <lesslybarba29@gmail.com>
2025-04-13 16:34:42 -0500

    prueba

--- src/router/RoutesApp.jsx
+++ src/router/RoutesApp.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { HomeScreen } from "../pages/home/HomeScreen"
 
-
 export const RoutesApp = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeScreen />} />
+        <Route path="/" element={< HomeScreen />} />
       </Routes>
     </BrowserRouter>
   )
